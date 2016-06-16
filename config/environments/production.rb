30924a0601ab0a4f09506fc97d7f2b330c634079
uraway <masato.u0107@gmail.com>
2016-06-16 18:32:30 +0900

    fix bug

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,7 +72,7 @@ Rails.application.configure do
     :authentication =>        :plan,
     :user_name =>             ENV['SENDGRID_USERNAME'],
     :password =>              ENV['SENDGRID_PASSWORD'],
-    :domain =>                'heroku.com'
+    :domain =>                'heroku.com',
     :enable_starttls_auto =>  true
   }
 
